51d082fb2f7307c4175083bd598f9ab4487767c5
Alex toni <aortiz@geosatelital.com.pe>
2021-11-28 23:58:01 -0500

    ARCHIVO v3

--- p_archivo.php
+++ p_archivo.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.css" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.0/sweetalert2.js"></script>
+</head>
+
+<body>
+    <script>
+
+    </script>
+</body>
+
+</html>
 <?php
 include("conexion.php");
 
@@ -15,9 +33,25 @@ if (isset($_COOKIE["codfut"])) {
 
                 $c = $_COOKIE["codfut"];
                 $sql = "update fut set ruta ='$destino' where  codfut ='$c'";
+                $sql1 = "select * from fut where codfut ='$c'";
                 mysqli_query($cn, $sql);
-                echo "El archivo " . basename($_FILES["archivo"]["name"]) . " Se subio correctamente";
-            } else {
+                $fila = mysqli_query($cn, $sql1);
+                $r = mysqli_fetch_array($fila);
+                $pa = $r["passfut"];
+                echo "<script> swal({
+                    title: '¡Credenciales Anotar!',
+                    html: 'Codigo :$c<br> Contraseña :$pa',
+                    type: 'success',
+                  },function(isConfirm){
+                    alert('ok');
+              });
+              $('.swal2-confirm').click(function(){
+                    window.location.href = 'index.php';
+              });
+                  
+                                  </script>";
+                 
+                } else {
                 echo "Error al cargar el archivo";
             }
         }
@@ -31,3 +65,6 @@ if (isset($_COOKIE["codfut"])) {
     header('location: index.php');
     echo "<script>alert('Porfavor Primero Guarde su Informacion!!');</script>";
 }
+
+
+?>
