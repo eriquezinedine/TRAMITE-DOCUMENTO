868dc732906c90538eb8a3c4c888df0cbe7f04f7
eriquezinedine <eriquezinedine@gmail.com>
2021-12-01 21:32:07 -0500

    alex es el puto amo

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <script src='./app.js'></script>
     <script src='./js/menu.js'></script>
     <script src='./js/detalle_expediente.js'></script>
+    <script src='./js/administrar.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
---
+++ js/administrar.js
@@ -0,0 +1,35 @@
+const btn_admin = document.querySelector('.btn_admin');
+const admin_despliegue= document.getElementById('admin_despliegue');
+const detalle_icono = document.querySelector('.detalle_icono');
+
+btn_admin.addEventListener('click', ()=>{
+    admin_despliegue.classList.toggle('efect')
+})
+
+detalle_icono.addEventListener('click', ()=>{admin_despliegue.classList.toggle('efect')})
+
+setTimeout(() => {
+
+}, 500);
+
+const estado_pintar = document.querySelectorAll('.estado_pintar');
+console.log(estado_pintar)
+
+estado_pintar.forEach(element=>{
+    let puntero = (element.textContent);
+    switch (puntero) {
+        case 'RECIBIDO':
+            element.parentElement.style.background= '#D0F0D6'
+            element.parentElement.style.color= '#23233C'
+            element.parentElement.children[7].children[0].style.color= '#23233C'
+            break;
+
+        case 'FINALIZADO':
+            element.parentElement.style.background='#8A1625 !important'
+            break;
+    
+        default:
+            element.parentElement.style.background='#F2F6FF'
+            break;
+    }
+})
